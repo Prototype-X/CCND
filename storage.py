2c3f69f1588075311122c7f9f6acbf85bc1f05d1
maximus <maxim.titaruk@ya.ru>
2017-07-18 16:53:51 +0300

    add class GitStorage

--- storage.py
+++ storage.py
@@ -3,6 +3,7 @@
 # __author__ = 'maximus'
 
 import os
+from datetime import datetime
 from shutil import copy2, make_archive, rmtree
 import logging
 logger = logging.getLogger('CCND')
@@ -44,3 +45,34 @@ class FileStorage(object):
     def archive(self):
         make_archive(self.path_session, 'gztar', self.dir_root, self.dir_session)
         rmtree(self.path_session)
+
+
+class GitStorage(object):
+    def __init__(self, path):
+        self.dir_root = path
+        self.dir_session = None
+        self.path_session = None
+        logger.info('Init file storage')
+
+    def add_to_storage(self, fname, data):
+        pass
+
+    def add_to_storage_from_tftp(self, fname, path_to_tftp):
+        tftp = path_to_tftp + '/' + fname + '.cfg'
+        storage = self.path_session + '/' + fname + '.cfg'
+        try:
+            copy2(tftp, storage)
+            os.remove(tftp)
+        except FileNotFoundError as err:
+            print(err)
+            return False
+        return True
+
+    def new_session(self):
+        self.dir_session = 'backup-{}'.format(datetime.now().strftime("%Y-%m-%dT%H:%M"))
+        self.path_session = '{}/{}'.format(self.dir_root, self.dir_session)
+        os.makedirs(self.path_session, exist_ok=True)
+
+    def close_session(self):
+        self.archive()
+        self.path_session = None
